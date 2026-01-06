caaf1be5beaf7cd8f2b665a83399b4c1e04e2b47
Shawn Ray <Asmodasis@gmail.com>
2026-01-06 14:58:52 -0800

    Workflow edit

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -390,7 +390,7 @@ std::string PlutotvData::GetChannelStreamURL(int uniqueId){
 }
 
 PVR_ERROR PlutotvData::GetChannelGroupsAmount(int& amount){
-  kodi::Log(ADDON_LOG_DEBUG, "[GetChannelGroupsAmount] INSIDE %s", __FUNCTION__);
+  kodi::Log(ADDON_LOG_ERROR, "[GetChannelGroupsAmount] INSIDE");
   try{
     amount = 0; 
     if(m_channels.size() == 0 || m_channels.size() < 0){
@@ -420,7 +420,7 @@ PVR_ERROR PlutotvData::GetChannelGroupsAmount(int& amount){
 }
 
 PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroupsResultSet& results){
-  kodi::Log(ADDON_LOG_DEBUG, "[GetChannelGroups] INSIDE %s", __FUNCTION__);
+  kodi::Log(ADDON_LOG_ERROR, "[GetChannelGroups] INSIDE");
   try{
     if(radio){
       throw PVR_ERROR_NOT_IMPLEMENTED; // no radio support
@@ -462,7 +462,7 @@ PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroup
 
 PVR_ERROR PlutotvData::GetChannelGroupMembers(const kodi::addon::PVRChannelGroup& group,
                                               kodi::addon::PVRChannelGroupMembersResultSet& results){
-  kodi::Log(ADDON_LOG_DEBUG, "[GetChannelGroupMembers] INSIDE %s", __FUNCTION__);
+  kodi::Log(ADDON_LOG_ERROR, "[GetChannelGroupMembers] INSIDE");
   try{
     //for (const auto& grp : m_Groups)
     for(unsigned int i = 0; i < (int)m_Groups.size(); ++i){
