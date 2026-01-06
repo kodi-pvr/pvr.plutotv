59f5e3a5d5aa076731d30de28f995c9778d9e7cc
Shawn Ray <Asmodasis@gmail.com>
2026-01-05 21:59:37 -0800

    testing capabilities

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -60,6 +60,8 @@ PVR_ERROR PlutotvData::GetCapabilities(kodi::addon::PVRCapabilities& capabilitie
   // supports channel grouping
   capabilities.SetSupportsChannelGroups(true);
   //capabilities.SetSupportsRecordings(true);
+  if(this->GetSupportsChannelGroups())
+     kodi::Log(ADDON_LOG_DEBUG, "[GetCapabilities] Channel Grouping is True;");
   return PVR_ERROR_NO_ERROR;
 }
 
