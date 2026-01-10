30e7c2cf98fa9d2953cc1291a13ce799537c3510
Shawn Ray <Asmodasis@gmail.com>
2026-01-10 12:53:34 -0800

    core dump search 8

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -401,7 +401,7 @@ PVR_ERROR PlutotvData::GetChannelGroupsAmount(int& amount){
 }
 
 PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroupsResultSet& results){
-  
+  /*
   kodi::addon::PVRChannelGroup group;
   group.SetIsRadio(false);
   //group.SetGroupName(m_channels[categoryCounter].strGroupName);
@@ -410,7 +410,7 @@ PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroup
   categoryCounter++;
   // Give it now to Kodi
   results.Add(group);
-  
+  */
   return PVR_ERROR_NO_ERROR;
     
 }
