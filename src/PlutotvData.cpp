bffffa6e82cb302ae873a7227710590e8b765916
Shawn Ray <Asmodasis@gmail.com>
2026-01-09 21:20:08 -0800

    searching for core dump 5

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -55,7 +55,7 @@ ADDON_STATUS PlutotvData::SetSetting(const std::string& settingName,
 
 PVR_ERROR PlutotvData::GetCapabilities(kodi::addon::PVRCapabilities& capabilities){
   kodi::Log(ADDON_LOG_DEBUG, "%s - GetCapabilities is being run", __FUNCTION__);
-  capabilities.SetSupportsChannelGroups(true);
+  //capabilities.SetSupportsChannelGroups(true);
   capabilities.SetSupportsTimers(false);
   //capabilities.SetSupportsRecordings(true);
   capabilities.SetSupportsEPG(true);
