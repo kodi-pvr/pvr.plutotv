5392b1673767e4870d6bb75e26d1984c73567879
Shawn Ray <Asmodasis@gmail.com>
2026-01-10 12:18:20 -0800

    check if getChannelGroupsAmount runs first

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -42,7 +42,7 @@ std::string PlutotvData::HttpGet(const std::string& url){
 
 
 ADDON_STATUS PlutotvData::Create(){
-  
+
   kodi::Log(ADDON_LOG_DEBUG, "%s - Creating the pluto.tv PVR add-on", __FUNCTION__);
   return ADDON_STATUS_OK;
 }
@@ -396,11 +396,12 @@ std::string PlutotvData::GetChannelStreamURL(int uniqueId){
 
 PVR_ERROR PlutotvData::GetChannelGroupsAmount(int& amount){
   amount = categoryCounter;
+  kodi::Log(ADDON_LOG_DEBUG, "[GetChannelGroupsAmount] categoryCounter is %s", categoryCounter.c_str());
   return PVR_ERROR_NO_ERROR;
 }
 
 PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroupsResultSet& results){
-  /*
+  
   kodi::addon::PVRChannelGroup group;
   group.SetIsRadio(false);
   //group.SetGroupName(m_channels[categoryCounter].strGroupName);
@@ -409,7 +410,7 @@ PVR_ERROR PlutotvData::GetChannelGroups(bool radio, kodi::addon::PVRChannelGroup
   categoryCounter++;
   // Give it now to Kodi
   results.Add(group);
-  */
+  
   return PVR_ERROR_NO_ERROR;
     
 }
