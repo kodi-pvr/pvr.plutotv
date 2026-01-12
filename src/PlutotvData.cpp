d4d032dcd97b2b5f61e7cc28b4583ec0e1bc1c07
Shawn Ray <Asmodasis@gmail.com>
2026-01-11 17:39:32 -0800

    comment out kodi log print

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -228,7 +228,7 @@ bool PlutotvData::LoadChannelsData(){
         // unique group located, store it to access easier later
         uniqueGroupList.emplace_back(std::to_string(channel.at("category")));
         categoryCounter++;
-        kodi::Log(ADDON_LOG_DEBUG, "[channel] uniqueGroupList located: %s;",uniqueGroupList.back());
+        //kodi::Log(ADDON_LOG_DEBUG, "[channel] uniqueGroupList located: %s;",uniqueGroupList.back());
     }
     kodi::Log(ADDON_LOG_DEBUG, "[channel] Category: %s;",plutotv_channel.m_Groups.GetGroupName().c_str());
 
