1bd374f524e6f37fcd6036354805d837cc4eeddb
Shawn Ray <Asmodasis@gmail.com>
2026-01-14 19:14:50 -0800

    Update PlutotvData.cpp

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -234,6 +234,7 @@ bool PlutotvData::LoadChannelsData(){
 
     std::string logo;
 
+    /*
     if (GetSettingsColoredChannelLogos())
     {
       //if (channel.HasMember("colorLogoPNG"))
@@ -256,6 +257,9 @@ bool PlutotvData::LoadChannelsData(){
       logo = channel.at("logo").at("path");
       kodi::Log(ADDON_LOG_DEBUG, "[channel] logo (fallback): %s;", logo.c_str());
     }
+    */
+      logo = channel.at("featuredImage").at("path");
+      kodi::Log(ADDON_LOG_DEBUG, "[channel] logo (fallback): %s;", logo.c_str());
 
     plutotv_channel.strIconPath = logo;
     kodi::Log(ADDON_LOG_DEBUG, "[channel] iconpath: %s;", plutotv_channel.strIconPath.c_str());
@@ -629,6 +633,9 @@ PVR_ERROR PlutotvData::GetEPGForChannel(int channelUid,
           {
             //tag.SetIconPath(episode["thumbnail"]["path"].GetString());
             tag.SetIconPath(episode.at("thumbnail").at("path"));
+          }else{
+            // TODO; check if fix icon
+            tag.SetIconPath(episode.at("featuredImage").at("path"));
           }
 
 
