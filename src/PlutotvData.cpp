582e5e51ec976583eee4890088ce605e9857147e
Shawn Ray <Asmodasis@gmail.com>
2026-01-17 15:27:26 -0800

    GetRecordingStreamProperties logic fix

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -743,9 +743,9 @@ PVR_ERROR PlutotvData::GetRecordingStreamProperties(
     std::vector<kodi::addon::PVRStreamProperty>& properties)
 {
   kodi::Log(ADDON_LOG_DEBUG, "%s - GetRecordingStreamProperties is being run", __FUNCTION__);
-  std::string url = GetRecordingURL(recording);
-  SetStreamProperties(properties, url, false);
-  //properties.emplace_back(PVR_STREAM_PROPERTY_STREAMURL, GetRecordingURL(recording));
+  //std::string url = GetRecordingURL(recording);
+  //SetStreamProperties(properties, url, false);
+  properties.emplace_back(PVR_STREAM_PROPERTY_STREAMURL, GetRecordingURL(recording));
   return PVR_ERROR_NO_ERROR;
 }
 
