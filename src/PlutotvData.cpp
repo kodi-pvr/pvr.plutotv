474128ddca519df44da08d9e4bcb1aa760c35aef
Shawn Ray <Asmodasis@gmail.com>
2026-01-18 13:43:19 -0800

    Update PlutotvData.cpp

--- src/PlutotvData.cpp
+++ src/PlutotvData.cpp
@@ -712,7 +712,7 @@ PVR_ERROR PlutotvData::GetRecordings(bool deleted, kodi::addon::PVRRecordingsRes
   for (const auto& recording : m_recordings)
   {
     kodi::addon::PVRRecording kodiRecording;
-    kodiRecording.SetRecordingId(recording.iUniqueId);
+    kodiRecording.SetRecordingId(recording.plutotvID);
     kodiRecording.SetTitle(recording.plutotvID);
     kodiRecording.SetChannelName(recording.strChannelName);
     //kodiRecording.SetPlotOutline(recording.strPlotOutline);
@@ -812,7 +812,7 @@ std::string PlutotvData::GetRecordingURL(const kodi::addon::PVRRecording& record
   kodi::Log(ADDON_LOG_DEBUG, "%s - GetRecordingURL is being run", __FUNCTION__);
   for (const auto& thisRecording : m_recordings)
   {
-    if (thisRecording.iUniqueId == recording.GetRecordingId())
+    if (thisRecording.plutotvID == recording.GetRecordingId())
     {
       return thisRecording.strStreamURL;
     }
